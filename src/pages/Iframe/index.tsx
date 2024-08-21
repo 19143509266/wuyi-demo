c49538cdc46dde3b2c8889d24b5ea62ba0bf0156
五一 <982936872@qq.com>
2024-08-21 18:09:17 +0800

    mix

--- src/pages/Iframe/index.tsx
+++ src/pages/Iframe/index.tsx
@@ -1,39 +1,38 @@
-import React, { useEffect } from 'react'
+import PartitionChart, { ChartsConfig } from '../PartitionChart'
 
-const Iframe = () => {
-  const ref = React.useRef<HTMLIFrameElement>(null)
-
-  useEffect(() => {
-    const handleLoad = () => {
-      if (ref.current) {
-        console.log('发消息...')
-        // 向 iframe 发送消息
-        ref.current.contentWindow?.postMessage(
-          { param1: 'value1', param2: 'value2' }, // 发送的消息对象
-          'http://localhost:4869' // 目标 origin
-        )
-      }
+const Demo = () => {
+  const chartsConfig: ChartsConfig = [
+    {
+      type: 'column',
+      data: [
+        { month: '1月', value: 2.0 },
+        { month: '2月', value: 4.9 }
+        // 其他数据
+      ],
+      color: '#f8f106',
+      yAxisTitle: '蒸发量'
+    },
+    {
+      type: 'line',
+      data: [
+        { month: '1月', value: 2.6 },
+        { month: '2月', value: 5.9 }
+      ],
+      color: '#fe7576',
+      yAxisTitle: '降水量'
+    },
+    {
+      type: 'line',
+      data: [
+        { month: '1月', value: 13 },
+        { month: '2月', value: 43 }
+      ],
+      color: '#fe7576',
+      yAxisTitle: '降水量'
     }
+  ]
 
-    // 监听 iframe 的加载事件
-    if (ref.current) {
-      ref.current.addEventListener('load', handleLoad)
-
-      return () => {
-        ref.current?.removeEventListener('load', handleLoad)
-      }
-    }
-  }, [])
-
-  return (
-    <iframe
-      ref={ref}
-      src="http://localhost:4869/wuyi-demo/react-grid-layout"
-      style={{ width: '100%', height: '100%' }}
-      frameBorder="0"
-      title="Iframe Example"
-    ></iframe>
-  )
+  return <PartitionChart chartsConfig={chartsConfig} />
 }
 
-export default Iframe
+export default Demo
